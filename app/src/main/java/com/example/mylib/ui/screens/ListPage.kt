e1dbca8cbbbc603a8f0d61f433408cba92dc9fa0
Róbert A. Jack <robertalex00@hotmail.com>
2026-02-25 19:51:24 +0000

    implemented the navigation bar

--- app/src/main/java/com/example/mylib/ui/screens/ListPage.kt
+++ app/src/main/java/com/example/mylib/ui/screens/ListPage.kt
@@ -7,16 +7,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 
 @Composable
-fun HomeFeedPage(navController: NavController){
+fun ListPage(){
     Column(
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
+        horizontalAlignment =  Alignment.CenterHorizontally
     ){
-        Text("Welcome to you home feed")
+        Text("list page")
     }
-
 }
